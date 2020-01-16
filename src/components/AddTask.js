06885bac7a4ac3b31de7cc9c5320ba841c3f721f
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-01-16 20:07:46 +0100

    Task adding form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import './AddTask.css';
 
 
 class AddTask extends Component {
-    state = {  }
+    
+    minDate = new Date().toISOString().slice(0,10);
+    state = { 
+        text: '',
+        checked: false,
+        date: this.minDate
+    }
+
+    handleDate = (e) => {
+        this.setState({
+            date: e.target.value
+        })
+    }
     render() { 
+        
+        let maxDate = this.minDate.slice(0,4);
+        maxDate = maxDate + "-12-31";
         return ( 
             <div className="form" >
-                <div>Dodaj task</div>
-                <hr></hr>
+                <input type="text" placeholder="Dodaj zadanie" value={this.state.text} />
+                <input type="checkbox" checked={this.state.checked} id="important"/>
+                <label htmlFor="important">Priorytet</label><br/>
+                <label htmlFor="date">Czas wykonania</label>
+                <input type="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate}/><br/>
+                <button>Dodaj</button>
             </div>
          );
     }
--- src/components/App.js
+++ src/components/App.js
@@ -97,7 +97,7 @@ class App extends Component {
   render() { 
     return ( 
       <div className="App">
-        <h1>TODO APP</h1>
+        <h1>ToDo App</h1>
         <AddTask />
         <TaskList tasks={this.state.tasks} delete={this.deleteTask} change={this.changeTaskStatus} />
       </div>
