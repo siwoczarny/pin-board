be288615bdcff3e99a27aa84ab2f2a0efdcac68c
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-01-22 14:58:47 +0100

    Updates

---
+++ precache-manifest.caa50543e9f8517d9b8c516c3fdfb2a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf713796332d50710b7f9c8ec9a20777",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "32e153ecdfcfa176b1d8",
+    "url": "/to-do/static/css/main.91c5a194.chunk.css"
+  },
+  {
+    "revision": "6e9c8c84888a10390bcd",
+    "url": "/to-do/static/js/2.1c9a85eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do/static/js/2.1c9a85eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32e153ecdfcfa176b1d8",
+    "url": "/to-do/static/js/main.17d56a17.chunk.js"
+  },
+  {
+    "revision": "8b9f7010519e44a820b7",
+    "url": "/to-do/static/js/runtime-main.0ea6de14.js"
+  },
+  {
+    "revision": "edc8c0c0f10a7e2bdddee82cbc422497",
+    "url": "/to-do/static/media/background.edc8c0c0.jpg"
+  }
+]);
